695f33f3eb846e6f5656bb2a4973d4cfdbccc267
jackwuliang <jackwuliang521314@gmail.com>
2022-11-28 22:09:09 +0800

    fix test

--- src/views/basicPage/login/index.vue
+++ src/views/basicPage/login/index.vue
@@ -11,14 +11,45 @@ import { ref } from 'vue'
 
 // defineProps<{ msg: string }>()
 
-const count = ref(0)
+
+const count = ref(0) // 响应式api
+
+// const ree = (fun: any): any => {
+// 	let b = 0
+//   console.log(b,'b')
+// 	setTimeout(() => {
+// 		b = fun()
+//     console.log(b,'bbbbb')
+// 	}, 1000)
+// 	return b
+// }
+
+// let c = ree(() => 100)
+// console.log(c, 'ccccc');
+
+    const demoref = (getter:any)=> {
+      let refa = {
+         value: 0
+      }  // 这里ref为值类型
+      console.log('before',refa)
+      setTimeout(() => {
+        refa.value = getter()
+        console.log('after',refa)
+      }, 1000);
+      return refa
+    }
+    let refb
+    refb =  demoref (() => 100)
+    console.log('refb',refb)
+
+    
 
 </script>
 
 <style scoped>
 
-.read-the-docs {
-  color: #888;
+.card {
+  background-color: red;
 }
 
 </style>
